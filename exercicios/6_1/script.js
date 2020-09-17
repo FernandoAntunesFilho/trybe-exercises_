2b17602d685e171a614aaf91ab3f947f38a1151d
Fernando Antunes <fernando.antunes1@gmail.com>
2020-09-17 19:30:17 -0300

    inicio do formulário do ccurriculo até os estados

---
+++ exercicios/6_1/script.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+    let estadosBrasil=['Acre (AC)','Alagoas (AL)','Amapá (AP)','Amazonas (AM)','Bahia (BA)','Ceará (CE)','Distrito Federal (DF)','Espírito Santo (ES)','Goiás (GO)','Maranhão (MA)','Mato Grosso (MT)','Mato Grosso do Sul (MS)','Minas Gerais (MG)','Pará (PA)','Paraíba (PB)','Paraná (PR)','Pernambuco (PE)','Piauí (PI)','Rio de Janeiro (RJ)','Rio Grande do Norte (RN)','Rio Grande do Sul (RS)','Rondônia (RO)','Roraima(RR)','Santa Catarina (SC)','São Paulo (SP)','Sergipe (SE)','Tocantins (TO)'];
+let listaDeEstados=document.getElementById('estados')
+
+for (let i=0; i<estadosBrasil.length; i+=1){
+    let estado=document.createElement('option');
+    estado.innerText=estadosBrasil[i];
+    console.logf
+    listaDeEstados.appendChild(estado);
+}
+    
+}
