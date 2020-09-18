da7a070d2823b016fe74f2cc27dd26c5c8778bc8
Fernando Antunes <fernando.antunes1@gmail.com>
2020-09-17 22:05:37 -0300

    inicio da implementação do botão Enviar

--- exercicios/6_1/script.js
+++ exercicios/6_1/script.js
@@ -1,6 +1,7 @@
 window.onload=function(){
     let estadosBrasil=['Acre (AC)','Alagoas (AL)','Amapá (AP)','Amazonas (AM)','Bahia (BA)','Ceará (CE)','Distrito Federal (DF)','Espírito Santo (ES)','Goiás (GO)','Maranhão (MA)','Mato Grosso (MT)','Mato Grosso do Sul (MS)','Minas Gerais (MG)','Pará (PA)','Paraíba (PB)','Paraná (PR)','Pernambuco (PE)','Piauí (PI)','Rio de Janeiro (RJ)','Rio Grande do Norte (RN)','Rio Grande do Sul (RS)','Rondônia (RO)','Roraima(RR)','Santa Catarina (SC)','São Paulo (SP)','Sergipe (SE)','Tocantins (TO)'];
-let listaDeEstados=document.getElementById('estados')
+let listaDeEstados=document.getElementById('estados');
+let enviar=document.getElementById('botao-enviar');
 
 for (let i=0; i<estadosBrasil.length; i+=1){
     let estado=document.createElement('option');
@@ -8,5 +9,9 @@ for (let i=0; i<estadosBrasil.length; i+=1){
     console.logf
     listaDeEstados.appendChild(estado);
 }
+
+enviar.addEventListener('click',function(){
+    
+})
     
 }
